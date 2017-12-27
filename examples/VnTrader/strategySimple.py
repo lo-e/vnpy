3c665f3d1e27f749e1e8a5933cc708e3eb60db4c
loe <lo-e@outlook.com>
2017-12-27 21:09:23 +0800

    update

---
+++ examples/VnTrader/strategySimple.py
@@ -0,0 +1,78 @@
+# coding: utf8
+
+from vnpy.trader.vtConstant import EMPTY_STRING, EMPTY_FLOAT
+from vnpy.trader.app.ctaStrategy.ctaTemplate import (CtaTemplate,
+                                                     BarManager,
+                                                     ArrayManager)
+
+class SimpleStrategy(CtaTemplate):
+    """双指数均线策略Demo"""
+    className = 'SimpleStrategy'
+    author = 'loe'
+
+    # 策略参数
+    a = 100
+
+    # 策略变量
+    b = EMPTY_FLOAT
+    c = EMPTY_STRING
+
+    # 参数列表，保存了参数的名称
+    paramList = ['a']
+
+    # 变量列表，保存了变量的名称
+    varList = ['b']
+
+    # 同步列表，保存了需要保存到数据库的变量名称
+    syncList = ['c']
+
+    # ----------------------------------------------------------------------
+    def __init__(self, ctaEngine, setting):
+        """Constructor"""
+        super(SimpleStrategy, self).__init__(ctaEngine, setting)
+
+    # ----------------------------------------------------------------------
+    def onInit(self):
+        """初始化策略（必须由用户继承实现）"""
+        self.writeCtaLog(u'simple演示策略初始化')
+        self.putEvent()
+
+    # ----------------------------------------------------------------------
+    def onStart(self):
+        """启动策略（必须由用户继承实现）"""
+        self.writeCtaLog(u'simple演示策略启动')
+        self.putEvent()
+
+    # ----------------------------------------------------------------------
+    def onStop(self):
+        """停止策略（必须由用户继承实现）"""
+        self.writeCtaLog(u'simple演示策略停止')
+        self.putEvent()
+
+    # ----------------------------------------------------------------------
+    def onTick(self, tick):
+        """收到行情TICK推送（必须由用户继承实现）"""
+        self.writeCtaLog(u'simple收到tick')
+
+    # ----------------------------------------------------------------------
+    def onBar(self, bar):
+        """收到Bar推送（必须由用户继承实现）"""
+        # 发出状态更新事件
+        self.putEvent()
+
+    # ----------------------------------------------------------------------
+    def onOrder(self, order):
+        """收到委托变化推送（必须由用户继承实现）"""
+        # 对于无需做细粒度委托控制的策略，可以忽略onOrder
+        pass
+
+    # ----------------------------------------------------------------------
+    def onTrade(self, trade):
+        """收到成交推送（必须由用户继承实现）"""
+        # 对于无需做细粒度委托控制的策略，可以忽略onOrder
+        pass
+
+    # ----------------------------------------------------------------------
+    def onStopOrder(self, so):
+        """停止单推送"""
+        pass
