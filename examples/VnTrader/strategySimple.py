c0416e146d24d379c4c6ebf2b4cb7fff6888f648
loe <lo-e@outlook.com>
2017-12-28 15:39:02 +0800

    update simple

--- examples/VnTrader/strategySimple.py
+++ examples/VnTrader/strategySimple.py
@@ -1,6 +1,6 @@
 # coding: utf8
 
-from vnpy.trader.vtConstant import EMPTY_STRING, EMPTY_FLOAT
+from vnpy.trader.vtConstant import EMPTY_STRING, EMPTY_FLOAT, EMPTY_UNICODE
 from vnpy.trader.app.ctaStrategy.ctaTemplate import (CtaTemplate,
                                                      BarManager,
                                                      ArrayManager)
@@ -11,28 +11,35 @@ class SimpleStrategy(CtaTemplate):
     author = 'loe'
 
     # 策略参数
-    a = 100
+    earnCount = 6 # 止盈点数
+    lossCount = 6 # 止损点数
+    posCount = 1 # 仓位控制
+    initDirection = u'多' # 初始头寸方向
 
     # 策略变量
-    b = EMPTY_FLOAT
-    c = EMPTY_STRING
+    lastDirection = EMPTY_UNICODE # 上次头寸方向
+    currentDirection = EMPTY_UNICODE # 当前头寸方向
+    posPrice = EMPTY_FLOAT # 当前头寸价格
 
     # 参数列表，保存了参数的名称
     paramList = ['name',
                  'className',
                  'author',
                  'vtSymbol',
-                 'a']
+                 'earnCount',
+                 'lossCount']
 
     # 变量列表，保存了变量的名称
     varList = ['inited',
                'trading',
                'pos',
-               'b']
+               'lastDirection',
+               'currentDirection',
+               'posPrice']
 
     # 同步列表，保存了需要保存到数据库的变量名称
     syncList = ['pos',
-                'c']
+                'posPrice']
 
     # ----------------------------------------------------------------------
     def __init__(self, ctaEngine, setting):
@@ -60,7 +67,60 @@ class SimpleStrategy(CtaTemplate):
     # ----------------------------------------------------------------------
     def onTick(self, tick):
         """收到行情TICK推送（必须由用户继承实现）"""
-        self.writeCtaLog(u'simple收到tick')
+        if not self.trading:
+            return
+
+        # 取消所有未成交的委托
+        self.cancelAll()
+
+        if self.pos > 0:
+            # 持有多头
+            self.currentDirection = u'多'
+
+            if tick.lastPrice >= (self.posPrice + self.earnCount):
+                # 止盈
+                self.sell(tick.lastPrice-6.0, abs(self.pos))
+                self.writeCtaLog(u'止盈委托%.2f' % tick.lastPrice)
+            if tick.lastPrice <= (self.posPrice - self.lossCount):
+                # 止损
+                self.sell(tick.lastPrice-6.0, abs(self.pos))
+                self.writeCtaLog(u'止损委托%.2f' % tick.lastPrice)
+        elif self.pos < 0:
+            #持有空头
+            self.currentDirection = u'空'
+
+            if tick.lastPrice <= (self.posPrice - self.earnCount):
+                # 止盈
+                self.cover(tick.lastPrice+6.0, abs(self.pos))
+                self.writeCtaLog(u'止盈委托%.2f' % tick.lastPrice)
+            if tick.lastPrice >= (self.posPrice + self.lossCount):
+                # 止损
+                self.cover(tick.lastPrice+6.0, abs(self.pos))
+                self.writeCtaLog(u'止损委托%.2f' % tick.lastPrice)
+        else:
+            #未持仓
+            if len(self.currentDirection):
+                self.lastDirection = self.currentDirection
+                self.currentDirection = EMPTY_UNICODE
+
+            toDirection = self.initDirection
+            if self.lastDirection == u'多':
+                toDirection = u'空'
+            elif self.lastDirection == u'空':
+                toDirection = u'多'
+
+            if toDirection == u'多':
+                self.buy(tick.lastPrice + 6.0, self.posCount)
+                self.writeCtaLog(u'多头开仓委托%.2f' % tick.lastPrice)
+            elif self.toDirection == u'空':
+                self.short(tick.lastPrice - 6.0, self.posCount)
+                self.writeCtaLog(u'多头空仓委托%.2f' % tick.lastPrice)
+            else:
+                # 默认做多
+                self.buy(tick.lastPrice + 6.0, self.posCount)
+                self.writeCtaLog(u'【默认】多头开仓委托%.2f' % tick.lastPrice)
+
+        self.putEvent()
 
     # ----------------------------------------------------------------------
     def onBar(self, bar):
@@ -77,8 +137,10 @@ class SimpleStrategy(CtaTemplate):
     # ----------------------------------------------------------------------
     def onTrade(self, trade):
         """收到成交推送（必须由用户继承实现）"""
-        # 对于无需做细粒度委托控制的策略，可以忽略onOrder
-        pass
+        # 对于无需做细粒度委托控制的策略，可以忽略onTrade
+        if trade.offset == u'开仓':
+            self.posPrice = trade.price
+        self.putEvent()
 
     # ----------------------------------------------------------------------
     def onStopOrder(self, so):
