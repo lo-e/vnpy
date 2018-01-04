5e0d6d7a7ef0071165e4cacaac2110b97f6ebbab
loe <lo-e@outlook.com>
2018-01-04 20:28:24 +0800

    update

--- examples/VnTrader/strategySimple.py
+++ examples/VnTrader/strategySimple.py
@@ -1,155 +1,155 @@
 # coding: utf8
 
-from datetime import datetime
+from datetime import time
 from vnpy.trader.vtConstant import EMPTY_STRING, EMPTY_FLOAT, EMPTY_UNICODE
-from vnpy.trader.app.ctaStrategy.ctaTemplate import (CtaTemplate,
-                                                     BarManager,
-                                                     ArrayManager)
+from vnpy.trader.app.ctaStrategy.ctaTemplate import (CtaTemplate)
 
 class SimpleStrategy(CtaTemplate):
-    """双指数均线策略Demo"""
+    """根据夜盘收盘前的价格趋势开仓入场，移动百分比离场"""
     className = 'SimpleStrategy'
-    author = 'loe'
+    author = u'loe'
 
     # 策略参数
-    earnCount = 6 # 止盈点数
-    lossCount = 6 # 止损点数
-    posCount = 1 # 仓位控制
-    initDirection = u'多' # 初始头寸方向
-    initDays = 1
+    '''
+    tradeSize = 1  # 交易数量
+    startTime = time(22, 58, 26)  # 趋势判断开始时间
+    endTime = time(22, 59, 56)  # 趋势判断截至时间
+    expireTime = time(8, 56)  # 夜盘时间
+    outPercent = 0.1  # 移动止盈止损百分比
+    '''
+
+    tradeSize = 1  # 交易数量
+    startTime = time(21, 2, 0)  # 趋势判断开始时间
+    endTime = time(21, 3, 0)  # 趋势判断截至时间
+    expireTime = time(8, 56)  # 信号过期时间
+    outPercent = 0.1  # 移动止盈止损百分比
 
     # 策略变量
-    lastDirection = EMPTY_UNICODE # 上次头寸方向
-    currentDirection = EMPTY_UNICODE # 当前头寸方向
-    posPrice = EMPTY_FLOAT # 当前头寸价格
+    startPrice = EMPTY_FLOAT  # 趋势判断开始价格
+    endPrice = EMPTY_FLOAT  # 趋势判断截至价格
+    highPrice = EMPTY_FLOAT  # 持仓后的最高价，为了多头止盈止损的计算
+    lowPrice = EMPTY_FLOAT  # 持仓后的最低价， 为了空头止盈止损的计算
 
     # 参数列表，保存了参数的名称
     paramList = ['name',
                  'className',
                  'author',
                  'vtSymbol',
-                 'earnCount',
-                 'lossCount']
+                 'tradeSize',
+                 'startTime',
+                 'endTime',
+                 'outPercent']
 
     # 变量列表，保存了变量的名称
     varList = ['inited',
                'trading',
                'pos',
-               'lastDirection',
-               'currentDirection',
-               'posPrice']
+               'highPrice',
+               'lowPrice']
 
-    # 同步列表，保存了需要保存到数据库的变量名称
+    # 同步列表
     syncList = ['pos',
-                'posPrice']
+                'highPrice',
+                'lowPrice']
 
     # ----------------------------------------------------------------------
     def __init__(self, ctaEngine, setting):
         """Constructor"""
         super(SimpleStrategy, self).__init__(ctaEngine, setting)
 
-    # ----------------------------------------------------------------------
+        # ----------------------------------------------------------------------
+
     def onInit(self):
         """初始化策略（必须由用户继承实现）"""
-        self.writeCtaLog(u'%s演示策略初始化' % self.name)
-
-        # 载入历史数据，并采用回放计算的方式初始化策略数值
-        initData = self.loadBar(self.initDays)
-        for bar in initData:
-            self.onBar(bar)
-
+        self.writeCtaLog(u'%s策略初始化' % self.name)
         self.putEvent()
 
     # ----------------------------------------------------------------------
     def onStart(self):
         """启动策略（必须由用户继承实现）"""
-        self.writeCtaLog(u'simple演示策略启动')
+        self.writeCtaLog(u'%s策略启动' % self.name)
         self.putEvent()
 
     # ----------------------------------------------------------------------
     def onStop(self):
         """停止策略（必须由用户继承实现）"""
-        self.writeCtaLog(u'simple演示策略停止')
+        self.writeCtaLog(u'%s策略停止' % self.name)
         self.putEvent()
 
     # ----------------------------------------------------------------------
     def onTick(self, tick):
         """收到行情TICK推送（必须由用户继承实现）"""
-        if not self.trading:
-            return
+        # print tick.datetime, ' '*2, tick.lastPrice
 
-        # 取消所有未成交的委托
+        # 撤销未成交的单
         self.cancelAll()
 
-        if self.pos > 0:
-            # 持有多头
-            self.currentDirection = u'多'
+        if (tick.datetime.time() >= self.expireTime) and (tick.datetime.time() < self.startTime):
+            # print tick.datetime, '='*16
+            self.startPrice = EMPTY_FLOAT
+            self.endPrice = EMPTY_FLOAT
 
-            if tick.lastPrice >= (self.posPrice + self.earnCount):
-                # 止盈
-                self.sell(tick.lastPrice-6.0, abs(self.pos))
-                self.writeCtaLog(u'止盈委托%.2f' % tick.lastPrice)
-            if tick.lastPrice <= (self.posPrice - self.lossCount):
-                # 止损
-                self.sell(tick.lastPrice-6.0, abs(self.pos))
-                self.writeCtaLog(u'止损委托%.2f' % tick.lastPrice)
+        if self.pos == 0:
+            self.highPrice = EMPTY_FLOAT
+            self.lowPrice = EMPTY_FLOAT
+            # 当前仓位为空才会做新的开仓信号判断
+            if (self.startPrice == 0) and (tick.datetime.time() >= self.startTime) and (
+                tick.datetime.time() < self.endTime):
+                self.startPrice = tick.lastPrice
+
+            if (self.endPrice == 0) and (tick.datetime.time() >= self.endTime):
+                self.endPrice = tick.lastPrice
+
+            if self.startPrice and self.endPrice:
+                sub = self.endPrice - self.startPrice
+                if sub > 0:
+                    # 开仓多头
+                    self.buy(tick.lastPrice + 10, self.tradeSize)  # 限价单
+                    # print u'委托多头开仓', tick.datetime, '*'*2, tick.lastPrice
+                elif sub < 0:
+                    # 开仓空头
+                    self.short(tick.lastPrice - 10, self.tradeSize)  # 限价单
+                    # print u'委托空头开仓', tick.datetime, '*'*2, tick.lastPrice
+        elif self.pos > 0:
+            # 持有多头仓位
+            if tick.lastPrice <= self.highPrice * (1 - self.outPercent / 100):
+                # 止盈止损
+                self.sell(tick.lastPrice - 10, abs(self.pos))  # 限价单
         elif self.pos < 0:
-            #持有空头
-            self.currentDirection = u'空'
+            # 持有空头仓位
+            if tick.lastPrice >= self.lowPrice * (1 + self.outPercent / 100):
+                # 止盈止损
+                self.cover(tick.lastPrice + 10, abs(self.pos))  # 限价单
 
-            if tick.lastPrice <= (self.posPrice - self.earnCount):
-                # 止盈
-                self.cover(tick.lastPrice+6.0, abs(self.pos))
-                self.writeCtaLog(u'止盈委托%.2f' % tick.lastPrice)
-            if tick.lastPrice >= (self.posPrice + self.lossCount):
-                # 止损
-                self.cover(tick.lastPrice+6.0, abs(self.pos))
-                self.writeCtaLog(u'止损委托%.2f' % tick.lastPrice)
-        else:
-            #未持仓
-            if len(self.currentDirection):
-                self.lastDirection = self.currentDirection
-                self.currentDirection = EMPTY_UNICODE
-
-            toDirection = self.initDirection
-            if self.lastDirection == u'多':
-                toDirection = u'空'
-            elif self.lastDirection == u'空':
-                toDirection = u'多'
-
-            if toDirection == u'多':
-                self.buy(tick.lastPrice + 6.0, self.posCount)
-                self.writeCtaLog(u'多头开仓委托%.2f' % tick.lastPrice)
-            elif toDirection == u'空':
-                self.short(tick.lastPrice - 6.0, self.posCount)
-                self.writeCtaLog(u'多头空仓委托%.2f' % tick.lastPrice)
-            else:
-                # 默认做多
-                self.buy(tick.lastPrice + 6.0, self.posCount)
-                self.writeCtaLog(u'【默认】多头开仓委托%.2f' % tick.lastPrice)
-
-        self.putEvent()
+        self.highPrice = max(self.highPrice, tick.lastPrice)
+        self.lowPrice = min(self.lowPrice, tick.lastPrice)
 
     # ----------------------------------------------------------------------
     def onBar(self, bar):
         """收到Bar推送（必须由用户继承实现）"""
         # 发出状态更新事件
-        self.writeCtaLog(u'barDatetime:%s close:%.2f' % (datetime.strftime(bar.datetime, '%Y-%m-%d %H-%M-%S.%f'), bar.close))
         self.putEvent()
 
     # ----------------------------------------------------------------------
     def onOrder(self, order):
         """收到委托变化推送（必须由用户继承实现）"""
-        # 对于无需做细粒度委托控制的策略，可以忽略onOrder
         pass
 
     # ----------------------------------------------------------------------
     def onTrade(self, trade):
-        """收到成交推送（必须由用户继承实现）"""
-        # 对于无需做细粒度委托控制的策略，可以忽略onTrade
-        if trade.offset == u'开仓':
-            self.posPrice = trade.price
+        # 发出状态更新事件
         self.putEvent()
+        if trade.offset == u'开仓':
+            self.highPrice = trade.price
+            self.lowPrice = trade.price
+        '''
+        print '#'*20, 'T R A D E', '#'*20
+        print trade.tradeTime
+        print trade.direction
+        print trade.offset
+        print trade.price
+        print self.pos, '\n'
+        '''
 
     # ----------------------------------------------------------------------
     def onStopOrder(self, so):
