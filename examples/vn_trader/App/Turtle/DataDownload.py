0a8c5708221ac16800828413607a97ef6a262595
loe <lo-e@outlook.com>
2019-09-19 04:22:48 +0800

    update

--- examples/vn_trader/App/Turtle/DataDownload.py
+++ examples/vn_trader/App/Turtle/DataDownload.py
@@ -2,6 +2,4 @@ from dataservice import TurtleDataDownloading
 
 if __name__ == '__main__':
     dataDownload = TurtleDataDownloading()
-    result, msg = dataDownload.download()
-    print(result)
-    print(msg)
+    dataDownload.download()
