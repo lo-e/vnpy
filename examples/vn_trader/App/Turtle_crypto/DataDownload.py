713ed86eb8d4b76a198c2fa5aa093e3e6f07381b
loe <lo-e@outlook.com>
2019-11-27 14:49:44 +0800

    update

--- examples/vn_trader/App/Turtle_crypto/DataDownload.py
+++ examples/vn_trader/App/Turtle_crypto/DataDownload.py
@@ -2,7 +2,7 @@ from dataservice import TurtleCryptoDataDownloading
 
 if __name__ == '__main__':
     contract_list = ['okef/btc.usd.q', 'okef/eth.usd.q', 'okef/eos.usd.q', 'okswap/btc.usd.td', 'okswap/eth.usd.td',
-                    'okswap/eos.usd.td']
+                    'okswap/eos.usd.td', 'okex/btc.usdt', 'okex/eth.usdt', 'okex/eos.usdt']
     dataDownload = TurtleCryptoDataDownloading()
     dataDownload.download(contract_list=contract_list, days=10)
     result, complete_msg, back_msg, lost_msg = dataDownload.generate(contract_list=contract_list, days=10)
