6c8fb17416ba9f355315a6f54a9f1c072030e0da
loe <lo-e@outlook.com>
2020-01-10 02:35:07 +0800

    crypto download update

--- examples/vn_trader/App/Turtle_crypto/DataDownload.py
+++ examples/vn_trader/App/Turtle_crypto/DataDownload.py
@@ -1,7 +1,7 @@
 from dataservice import TurtleCryptoDataDownloading
 
 if __name__ == '__main__':
-    #"""
+    """
     contract_list = ['okef/btc.usd.q', 'okef/eth.usd.q', 'okef/eos.usd.q', 'okswap/btc.usd.td', 'okswap/eth.usd.td',
                     'okswap/eos.usd.td', 'okex/btc.usdt', 'okex/eth.usdt', 'okex/eos.usdt']
     contract_list = ['okef/btc.usd.t', 'okef/btc.usd.n']
@@ -10,13 +10,13 @@ if __name__ == '__main__':
     dataDownload.download_from_onetoken(contract_list=contract_list, days=days)
     result, complete_msg, back_msg, lost_msg = dataDownload.generate_for_onetoken(contract_list=contract_list, days=days)
     print('\n\n' + lost_msg + back_msg)
-    #"""
-
     """
+
+    #"""
     contract_list = ['BTCUSD', 'ETHUSD', 'EOSUSD']
-    days = 1
+    days = 2
     dataDownload = TurtleCryptoDataDownloading()
     dataDownload.download_from_bybit(contract_list=contract_list, days=days)
     result, complete_msg, back_msg, lost_msg = dataDownload.generate_for_bybit(contract_list=contract_list, days=days)
     print('\n\n' + lost_msg + back_msg)
-    """
+    #"""
