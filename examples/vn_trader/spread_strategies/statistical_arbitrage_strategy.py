c6b405204e2b4138c45f1beffde597b1aea059f8
loe <lo-e@outlook.com>
2020-03-01 22:13:30 +0800

    update spread trade max_pos = 30

--- examples/vn_trader/spread_strategies/statistical_arbitrage_strategy.py
+++ examples/vn_trader/spread_strategies/statistical_arbitrage_strategy.py
@@ -25,7 +25,7 @@ class StatisticalArbitrageStrategy(SpreadStrategyTemplate):
     boll_window = 20
     boll_dev = 2
     open_value = 4
-    max_pos = 10
+    max_pos = 30
     payup = 10
     interval = 5
 
