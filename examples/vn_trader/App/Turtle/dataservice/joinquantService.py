00a159589b76a68dac1bfe84eef74afac768e956
loe <lo-e@outlook.com>
2020-03-11 17:05:03 +0800

    update

--- examples/vn_trader/App/Turtle/dataservice/joinquantService.py
+++ examples/vn_trader/App/Turtle/dataservice/joinquantService.py
@@ -23,7 +23,7 @@ auth('18521705317', '970720699')
 
 # 使用聚宽数据服务，添加新的品种必须这里添加代码
 EXCHANGE_SYMBOL_MAP = {'XSGE':['RB', 'HC', 'RU'],
-                       'XZCE':['SM', 'ZC', 'TA'],
+                       'XZCE':['SM', 'ZC', 'TA', 'CF'],
                        'XDCE':['J', 'A', 'I']}
 
 client = MongoClient('localhost', 27017, serverSelectionTimeoutMS=600)
