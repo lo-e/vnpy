5c9cbc6bf24f95344275fae6c5ebebc2aed6708d
loe <lo-e@outlook.com>
2020-03-23 10:48:16 +0800

    update

--- examples/spread_backtesting/run.py
+++ examples/spread_backtesting/run.py
@@ -10,21 +10,16 @@ from vnpy.trader.constant import Direction, Offset
 import csv
 
 def one():
-    name = 'CY'
-    symbol_up = 'CY2009'
-    symbol_down = 'CY2005'
-    symbol_active = 'CY2009'
-    min_volume = 1
+    name = 'OI'
+    symbol_up, symbol_down, symbol_active, size, price_tick, commission_rate = get_backtesting_params(name=name)
 
-    size = 5
-    price_tick = 5
-    commission_rate = 0.000
+    min_volume = 1
     slippage = 0
     capital = 200000
 
     strategy_params = {'boll_dev':5}
 
-    start = datetime(2019, 12, 20)
+    start = datetime(2020, 1, 20)
     end = datetime(2020, 12, 31)
 
     spread = SpreadData(
@@ -136,6 +131,64 @@ def one():
                 writer.writeheader()
                 # 写入csv文件
                 writer.writerows(resultList)
+
+def get_backtesting_params(name:str):
+    symbol_up = ''
+    symbol_down = ''
+    symbol_active = ''
+    size = 0
+    price_tick = 0
+    commission_rate = 0.000
+
+    if name == 'CY':
+        symbol_up = 'CY2009'
+        symbol_down = 'CY2005'
+        symbol_active = 'CY2009'
+        size = 5
+        price_tick = 5
+        commission_rate = 0.000
+
+    elif name == 'EG':
+        symbol_up = 'EG2009'
+        symbol_down = 'EG2005'
+        symbol_active = 'EG2009'
+        size = 10
+        price_tick = 1
+        commission_rate = 0.000
+
+    elif name == 'OI':
+        symbol_up = 'OI2005'
+        symbol_down = 'OI2009'
+        symbol_active = 'OI2009'
+        size = 10
+        price_tick = 1
+        commission_rate = 0.000
+
+    elif name == 'PB':
+        symbol_up = 'PB2005'
+        symbol_down = 'PB2006'
+        symbol_active = 'PB2006'
+        size = 5
+        price_tick = 5
+        commission_rate = 0.000
+
+    elif name == 'RM':
+        symbol_up = 'RM2009'
+        symbol_down = 'RM2005'
+        symbol_active = 'RM2009'
+        size = 10
+        price_tick = 1
+        commission_rate = 0.000
+
+    elif name == '':
+        symbol_up = ''
+        symbol_down = ''
+        symbol_active = ''
+        size = 0
+        price_tick = 0
+        commission_rate = 0.000
+
+    return symbol_up, symbol_down, symbol_active, size, price_tick, commission_rate
 
 if __name__ == '__main__':
     one()
