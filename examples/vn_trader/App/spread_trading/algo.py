dcd8f7d14b45856518f97da0a1e88580a6085a47
loe <lo-e@outlook.com>
2020-03-27 11:14:08 +0800

    【Only modify examples/vn_trader/App/spread_trading/algo.py】 optimize algo to trade active and passive at the same time

--- examples/vn_trader/App/spread_trading/algo.py
+++ examples/vn_trader/App/spread_trading/algo.py
@@ -51,15 +51,16 @@ class SpreadTakerAlgo(SpreadAlgoTemplate):
         # Hedge if active leg is not fully hedged
         if not self.check_hedge_finished():
             self.hedge_passive_legs()
+            self.hedge_active_legs()
             return
 
         # Otherwise check if should take active leg
         if self.direction == Direction.LONG:
             if self.spread.ask_price <= self.price:
-                self.take_active_leg()
+                self.take_active_passive_leg()
         else:
             if self.spread.bid_price >= self.price:
-                self.take_active_leg()
+                self.take_active_passive_leg()
 
     def on_order(self, order: OrderData):
         """"""
@@ -74,6 +75,7 @@ class SpreadTakerAlgo(SpreadAlgoTemplate):
         # Hedge passive legs if necessary
         if not self.check_hedge_finished():
             self.hedge_passive_legs()
+            self.hedge_active_legs()
 
     def on_trade(self, trade: TradeData):
         """"""
@@ -88,6 +90,8 @@ class SpreadTakerAlgo(SpreadAlgoTemplate):
         """"""
         # Calculate spread order volume of new round trade
         spread_volume_left = self.target - self.traded
+        if not spread_volume_left:
+            return
 
         if self.direction == Direction.LONG:
             spread_order_volume = self.spread.ask_volume
@@ -115,6 +119,45 @@ class SpreadTakerAlgo(SpreadAlgoTemplate):
             leg_order_volume
         )
 
+    """ modify by loe """
+    # 主动退被动腿同时委托，只适用于单条被动腿的策略
+    def take_active_passive_leg(self):
+        """"""
+        # Calculate spread order volume of new round trade
+        spread_volume_left = self.target - self.traded
+        if not spread_volume_left:
+            return
+
+        if self.direction == Direction.LONG:
+            spread_order_volume = self.spread.ask_volume
+            spread_order_volume = min(spread_order_volume, spread_volume_left)
+        else:
+            spread_order_volume = -self.spread.bid_volume
+            spread_order_volume = max(spread_order_volume, spread_volume_left)
+
+        # Calculate active leg order volume
+        active_leg_order_volume = self.spread.calculate_leg_volume(
+            self.spread.active_leg.vt_symbol,
+            spread_order_volume
+        )
+
+        passive_leg = self.spread.passive_legs[0]
+        passive_leg_order_volume = self.spread.calculate_leg_volume(
+            passive_leg.vt_symbol,
+            spread_order_volume
+        )
+
+        # Send active leg order
+        self.send_leg_order(
+            self.spread.active_leg.vt_symbol,
+            active_leg_order_volume
+        )
+
+        self.send_leg_order(
+            passive_leg.vt_symbol,
+            passive_leg_order_volume
+        )
+
     def hedge_passive_legs(self):
         """
         Send orders to hedge all passive legs.
@@ -139,9 +182,46 @@ class SpreadTakerAlgo(SpreadAlgoTemplate):
                 hedge_volume
             )
 
+            """ modify by loe """
+            if abs(passive_traded) >= abs(passive_target):
+                return
+
             leg_order_volume = passive_target - passive_traded
             if leg_order_volume:
                 self.send_leg_order(leg.vt_symbol, leg_order_volume)
+
+    def hedge_active_legs(self):
+        # 只适用于一条被动腿的策略
+        """
+        Send orders to hedge active legs.
+        """
+        # Calcualte spread volume to hedge
+        passive_leg = self.spread.passive_legs[0]
+        passive_traded = self.leg_traded[passive_leg.vt_symbol]
+        passive_traded = round_to(passive_traded, self.spread.min_volume)
+
+        hedge_volume = self.spread.calculate_spread_volume(
+            passive_leg.vt_symbol,
+            passive_traded
+        )
+
+        # Calculate passive leg target volume and do hedge
+        active_leg = self.spread.active_leg
+        active_traded = self.leg_traded[active_leg.vt_symbol]
+        active_traded = round_to(active_traded, self.spread.min_volume)
+
+        active_target = self.spread.calculate_leg_volume(
+            active_leg.vt_symbol,
+            hedge_volume
+        )
+
+        """ modify by loe """
+        if abs(active_traded) >= abs(active_target):
+            return
+
+        leg_order_volume = active_target - active_traded
+        if leg_order_volume:
+            self.send_leg_order(active_leg.vt_symbol, leg_order_volume)
 
     def send_leg_order(self, vt_symbol: str, leg_volume: float):
         """"""
