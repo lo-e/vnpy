b053f994f020ed4242517bef2c071149ce64a67e
loe <lo-e@outlook.com>
2020-08-21 10:52:29 +0800

    update install.bat add 清华源

--- install.bat
+++ install.bat
@@ -1,15 +1,13 @@
 :: Upgrade pip & setuptools
-python -m pip install --upgrade pip setuptools
+python -m pip install -i https://pypi.tuna.tsinghua.edu.cn/simple --upgrade pip setuptools
 
 ::Install rqdatac、talib、ibapi（requirements.txt可直接下载）
-pip install --extra-index-url https://rquser:ricequant99@py.ricequant.com/simple/ rqdatac 
-python -m pip install https://pip.vnpy.com/colletion/TA_Lib-0.4.17-cp37-cp37m-win_amd64.whl
+pip install -i https://pypi.tuna.tsinghua.edu.cn/simple --extra-index-url https://rquser:ricequant99@py.ricequant.com/simple/ rqdatac 
+python -m pip install -i https://pypi.tuna.tsinghua.edu.cn/simple https://pip.vnpy.com/colletion/TA_Lib-0.4.17-cp37-cp37m-win_amd64.whl
 ::python -m pip install https://pip.vnpy.com/colletion/ibapi-9.75.1-001-py3-none-any.whl
 
 ::Install Python Modules
-python -m pip install -r requirements.txt
-::下载很慢的话通过更换清华源下载改善
-::pip install -i https://pypi.tuna.tsinghua.edu.cn/simple -r requirements.txt
+python -m pip install -i https://pypi.tuna.tsinghua.edu.cn/simple -r requirements.txt
 
 :: Install vn.py
 ::python -m pip install .
