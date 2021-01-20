5df79b8e62ee31c585990d2902967375f4a9754f
loe <lo-e@outlook.com>
2021-01-20 12:25:24 +0800

    fix

--- examples/vn_trader/App/Turtle/dataservice/joinquantService.py
+++ examples/vn_trader/App/Turtle/dataservice/joinquantService.py
@@ -365,8 +365,15 @@ def jq_get_and_save_dominant_symbol(underlying_symbol:str, target_date:datetime)
         for the_data in open_interest_list:
             # 若合约持仓量大于当前主力合约持仓量的1.1倍时，新主力产生
             if the_data.open_interest > last_dominant_open_interest * 1.1:
-                new_dominant_symbol = the_data.symbol
-                new_dominant_data_list.append(the_data)
+                new_verified = True
+                if new_dominant_data_list:
+                    last_data = new_dominant_data_list[0]
+                    if last_data.open_interest >= the_data.open_interest:
+                        new_verified = False
+
+                if new_verified:
+                    new_dominant_symbol = the_data.symbol
+                    new_dominant_data_list = [the_data]
 
         if len(new_dominant_data_list) > 1:
             # 有多个锌主力产生
