5d3e2dfb214bcafd13de62aebe9971ab382944b0
loe <lo-e@outlook.com>
2021-12-20 12:48:53 +0800

    fix jqdatasdk auth

--- examples/vn_trader/App/Turtle/dataservice/joinquantService.py
+++ examples/vn_trader/App/Turtle/dataservice/joinquantService.py
@@ -21,7 +21,8 @@ from collections import defaultdict
 
 # 聚宽账号登陆
 if not is_auth():
-    auth('18521705317', '970720699')
+    #auth('18521705317', '970720699')
+    pass
 
 # 使用聚宽数据服务，添加新的品种必须这里添加代码
 EXCHANGE_SYMBOL_MAP = {'XSGE':['RB', 'HC', 'RU', 'CU', 'PB', 'SN', 'SP', 'WR', 'ZN', 'AL', 'NI', 'AG', 'AU', 'BU', 'FU'],
