9a630bd30df1b84f3a22cc6bd84a4838139b391e
loe <lo-e@outlook.com>
2022-01-11 10:17:33 +0800

    update

--- examples/vn_trader/App/Turtle_crypto/dataservice/downloadData.py
+++ examples/vn_trader/App/Turtle_crypto/dataservice/downloadData.py
@@ -18,34 +18,6 @@ class TurtleCryptoDataDownloading(object):
     def __init__(self):
         pass
 
-    def download_from_onetoken(self, contract_list, days=1):
-        #"""
-        # 先删除原有文件夹，包括其中所有内容
-        csv_path = get_csv_path()
-        if os.path.exists(csv_path):
-            shutil.rmtree(csv_path)
-
-        # 获取bar数据
-        duration = '1m'
-        start = datetime.now() - timedelta(days=days)
-        start = datetime(start.year, start.month, start.day)
-        until = datetime.now() - timedelta(0)
-        until = datetime(until.year, until.month, until.day)
-
-        since = start
-        while since <= until:
-            for contract in contract_list:
-                print(f'下载数据：{since}\t{contract}')
-                get_bar_data(contract=contract, since=datetime.strftime(since, '%Y-%m-%d'),
-                             until=datetime.strftime(since + timedelta(1), '%Y-%m-%d'), duration=duration)
-            since += timedelta(1)
-        #"""
-
-        # 1m数据入数据库
-        print('\n====== 1m数据入数据库 ======')
-        engine = CSVs1TokenBarLocalEngine(duration='1m')
-        engine.startWork()
-
     def download_from_bybit(self, contract_list, days=1):
         #"""
         # 先删除原有文件夹，包括其中所有内容
@@ -108,40 +80,6 @@ class TurtleCryptoDataDownloading(object):
         print('\n====== 1D数据入数据库 ======')
         engine = CSVsFTXBarLocalEngine(duration=interval)
         engine.startWork()
-
-    def generate_for_onetoken(self, contract_list, days=1):
-        result = True
-        complete_msg = ''
-        back_msg = ''
-        lost_msg = ''
-
-        if not contract_list:
-            return False, complete_msg, back_msg, lost_msg
-
-        # 1m数据合成Daily数据
-        print('\n====== 1m数据合成Daily数据 ======')
-        engine = BarLocalEngine()
-
-        from_day = datetime.now() - timedelta(days=days)
-        to_day = datetime.now()
-        start_date = datetime.strptime(f'{from_day.year}-{from_day.month}-{from_day.day} 08:00:00', '%Y-%m-%d %H:%M:%S')
-        end_date = datetime.strptime(f'{to_day.year}-{to_day.month}-{to_day.day} 07:59:00', '%Y-%m-%d %H:%M:%S')
-
-        re_list = []
-        for contract in contract_list:
-            elements = contract.split('/')
-            symbol = '.'.join([elements[-1], elements[0]]).upper()
-            re, c_msg, b_msg, l_msg = engine.Crypto_1Min_Daily(symbol=symbol, start_date=start_date, end_date=end_date)
-            re_list.append(re)
-            complete_msg += c_msg + '\n\n'
-            back_msg += b_msg + '\n\n'
-            lost_msg += l_msg + '\n\n'
-
-        for re in re_list:
-            if not re:
-                result = False
-                break
-        return result, complete_msg, back_msg, lost_msg
 
     def generate_for_bybit(self, contract_list, days=1):
         result = True
