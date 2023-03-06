22d7dc460b101e9fb9aa18e10e55564fc76f53b6
loe <lo-e@outlook.com>
2023-03-06 12:12:39 +0800

    formatter

--- examples/utilities/rumi.py
+++ examples/utilities/rumi.py
@@ -4,20 +4,26 @@ import numpy as np
 import talib
 from vnpy.trader.constant import Direction
 
+
 class Rumi(object):
     """
     RUMI指标采用了对均线偏离度平滑处理
     """
-    def __init__(self,
-                 fast_window: int = 7,
-                 slow_window: int = 14,
-                 rumi_window: int = 3,
-                 show_window: int = 20):
+
+    def __init__(
+        self,
+        fast_window: int = 7,
+        slow_window: int = 14,
+        rumi_window: int = 3,
+        show_window: int = 20,
+    ):
         self.fast_window = fast_window
         self.slow_window = slow_window
         self.rumi_window = rumi_window
         self.show_window = show_window
-        self.am = ArrayManager(self.show_window + self.slow_window + self.rumi_window - 2)
+        self.am = ArrayManager(
+            self.show_window + self.slow_window + self.rumi_window - 2
+        )
 
         self.inited = False
         self.rma: np.ndarray = np.array([])
@@ -32,7 +38,7 @@ class Rumi(object):
             slow_array: np.ndarray = self.am.wma(n=self.slow_window, array=True)
             diff_array: np.ndarray = fast_array - slow_array
             rumi_array: np.ndarray = talib.SMA(diff_array, self.rumi_window)
-            self.rma = rumi_array[len(rumi_array)-self.show_window:]
+            self.rma = rumi_array[len(rumi_array) - self.show_window :]
             self.generate_signal()
 
     def generate_signal(self) -> None:
@@ -56,7 +62,7 @@ class Rumi(object):
                 # 当前处于0轴
                 index += 1
                 continue
-            
+
             if last_direction * current_direction < 0:
                 self.cross_count += 1
                 if index == len(self.rma) - 1:
