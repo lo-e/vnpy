dec314199c3c745dc0487d264f15d106c2cc39f0
loe <lo-e@outlook.com>
2023-04-11 18:07:08 +0800

    update

--- examples/TurtleStrategy/turtleEngine.py
+++ examples/TurtleStrategy/turtleEngine.py
@@ -13,8 +13,8 @@ from pymongo import MongoClient
 from vnpy.trader.object import BarData
 from vnpy.trader.constant import Direction, Exchange
 
-# from turtleStrategy import TurtlePortfolio
-from turtleStrategyDev import TurtlePortfolio
+from turtleStrategy import TurtlePortfolio
+# from turtleStrategyDev import TurtlePortfolio
 
 from vnpy.app.cta_strategy.base import DAILY_DB_NAME, MINUTE_DB_NAME, HOUR_DB_NAME, MinuteDataBaseName, HourDataBaseName
 import pandas as pd
