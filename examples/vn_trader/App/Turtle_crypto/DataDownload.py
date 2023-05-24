d643784bef659ac8cf9d293594bdf70848b69d99
loe <lo-e@outlook.com>
2023-05-24 12:59:10 +0800

    update

--- examples/vn_trader/App/Turtle_crypto/DataDownload.py
+++ examples/vn_trader/App/Turtle_crypto/DataDownload.py
@@ -30,10 +30,10 @@ if __name__ == '__main__':
         print(f"即将下载总计：{len(contract_list)}")
 
     # 起止日期
-    # days = 6
-    # to_date = datetime.now() + timedelta(days=2)
-    days = (datetime.now() - datetime.strptime('2020-01-01', '%Y-%m-%d')).days
-    to_date = datetime.strptime('2023-12-31', '%Y-%m-%d')
+    days = 6
+    to_date = datetime.now() + timedelta(days=2)
+    # days = (datetime.now() - datetime.strptime('2020-01-01', '%Y-%m-%d')).days
+    # to_date = datetime.strptime('2023-12-31', '%Y-%m-%d')
 
     # 是否从数据库最新数据日期开始
     from_data_base = True
