be218904c254f76bab19e7c7ba8e030504d1baf7
loe <lo-e@outlook.com>
2023-05-27 23:59:15 +0800

    update

--- examples/MartingStrategy/TEST.py
+++ examples/MartingStrategy/TEST.py
@@ -30,15 +30,19 @@ def calculate_phase_loss(phase_count:int, increase_type:int=1):
         loss_after = round(loss_after, 2)
         loss_after = f"{loss_after}%"
 
-        print(f"\n第{i+1}阶段\n持仓价值：{value_after}\n持仓数量：{value_after/price_after}\n持仓均价：{price_after}\n加仓前亏损：{loss_before}\n加仓后亏损：{loss_after}")
+        print(f"\n第{i+1}阶段\n持仓价值：{value_after}\n持仓数量：{value_after/price_after}\n持仓均价：{price_after}\n成交价格：{trade_price}\n加仓前亏损：{loss_before}\n加仓后亏损：{loss_after}")
 
         value_before = value_after
         price_before = price_after
     
-    price_changed = ((price_before / init_price) - 1) * 100
-    price_changed = round(price_changed, 2)
-    price_changed = f"{price_changed}%"
-    print(f"\n\n============\n总共经历{phase_count}个阶段加仓\n初始持仓价值：{phase_value_list[0]}\t初始持仓价格：{init_price}\n最后持仓价值：{value_before}\t最后持仓价格：{price_before}\n价格变化：{price_changed}")
+    symbol_price_changed = ((trade_price / init_price) - 1) * 100
+    symbol_price_changed = round(symbol_price_changed, 2)
+    symbol_price_changed = f"{symbol_price_changed}%"
+
+    position_price_changed = ((price_before / init_price) - 1) * 100
+    position_price_changed = round(position_price_changed, 2)
+    position_price_changed = f"{position_price_changed}%"
+    print(f"\n\n============\n总共经历{phase_count}个阶段加仓\n初始持仓价值：{phase_value_list[0]}\t初始持仓价格：{init_price}\n最后持仓价值：{value_before}\t最后持仓价格：{price_before}\n持仓价格变化：{position_price_changed}\n合约价格变化：{symbol_price_changed}")
 
 if __name__ == "__main__":
     calculate_phase_loss(phase_count=6, increase_type=1)
