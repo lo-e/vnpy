8056cc9972763914b21019bee340a9361817417b
loe <lo-e@outlook.com>
2023-06-13 15:44:20 +0800

    update

--- examples/vn_trader/App/marting/martingPortfolio.py
+++ examples/vn_trader/App/marting/martingPortfolio.py
@@ -251,6 +251,8 @@ class MartingPortfolio(object):
 
     def notice_strategy_info(self):
         total = 0
+        loss_tick_count = 9
+        loss_tick_content = ""
         error_count = 0
         fit_content = ""
         fit_count = 0
@@ -263,6 +265,11 @@ class MartingPortfolio(object):
         for _, strategy in self.engine.strategies.items():
             # 总计
             total += 1
+
+            if not strategy.tick:
+                loss_tick_count += 1
+                symbol = strategy.vt_symbol.split(".")[0]
+                loss_tick_content += f"{symbol} "
 
             # 回测数据缺失的数量
             backtesting_status = (
@@ -320,14 +327,15 @@ class MartingPortfolio(object):
                         highlight_content += "\n\n" + "-" * 10 + "\n\n"
                         highlight_count += 1
 
+        main_content = f"策略总数：{total}\n行情缺失合约数：{loss_tick_count}\n行情缺失合约：{loss_tick_content}\n回测周期：{min_datetime} - {max_datetime}"
         # 邮件发送通知
         fit_content = (
-            f"\n策略总数：{total}\n回测周期：{min_datetime} - {max_datetime}\n" + fit_content
+            f"\n{main_content}\n" + fit_content
         )
         self.engine.send_email(msg=fit_content, subject=f"马丁策略组合状态信息【正常：{fit_count}】")
 
         un_fit_content = (
-            f"\n策略总数：{total}\n回测周期：{min_datetime} - {max_datetime}\n" + un_fit_content
+            f"\n{main_content}\n" + un_fit_content
         )
         self.engine.send_email(
             msg=un_fit_content, subject=f"马丁策略组合状态信息【非正常：{un_fit_count}】"
@@ -335,7 +343,7 @@ class MartingPortfolio(object):
 
         if error_count:
             error_subject = f"马丁策略组合状态信息【回测状态缺失数量：{error_count}】"
-            error_content = f"\n策略总数：{total}\n回测周期：{min_datetime} - {max_datetime}\n"
+            error_content = f"\n{main_content}\n"
             self.engine.send_email(
                 msg=error_content, subject=error_subject
             )
@@ -346,7 +354,7 @@ class MartingPortfolio(object):
         if highlight_count:
             highlight_subject = f"马丁策略组合状态信息【高等级追踪：{highlight_count}】"
             highlight_content = (
-                f"\n策略总数：{total}\n回测周期：{min_datetime} - {max_datetime}\n"
+                f"\n{main_content}\n"
                 + highlight_content
             )
             self.engine.send_email(
