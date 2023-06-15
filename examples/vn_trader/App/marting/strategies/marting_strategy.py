40af01d09b8db997009cc697d51990bccced3182
loe <lo-e@outlook.com>
2023-06-15 08:44:39 +0800

    update

--- examples/vn_trader/App/marting/strategies/marting_strategy.py
+++ examples/vn_trader/App/marting/strategies/marting_strategy.py
@@ -49,8 +49,6 @@ class MartingStrategy(CtaTemplate):
         "init_value_rate",
         "bottom_step",
         "top_step",
-        "is_backtesting",
-        "backtesting_wait",
         "symbol_price_tick",
         "symbol_min_volume",
         "backtesting_to",
