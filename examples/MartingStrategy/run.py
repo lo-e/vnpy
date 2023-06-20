9a9bb0cbf68bfb325cb8766ca8321232af5aa5d2
loe <lo-e@outlook.com>
2023-06-20 13:12:04 +0800

    update

--- examples/MartingStrategy/run.py
+++ examples/MartingStrategy/run.py
@@ -23,7 +23,7 @@ from vnpy.trader.utility import round_to
 def one():
     # 回测起始日期
     engine = BacktestingEngine()
-    start_dt = datetime(2023, 6, 1)
+    start_dt = datetime(2023, 6, 10)
     end_dt = datetime(2023, 12, 31)
     engine.setPeriod(start_dt, end_dt)
     figSavedName = ""
@@ -45,7 +45,7 @@ def one():
 
     # 回测历史数据文件
     backtesting_history_file = ""
-    backtesting_history_file = "2022-01-01_2023-06-10.json"
+    backtesting_history_file = "2022-01-01_2023-06-20.json"
 
     # 开始回测
     symbolList = []
