fb09973fa4aa44ab6bcfb5749e32a5f92b4b713a
loe <lo-e@outlook.com>
2023-06-22 11:36:20 +0800

    update

--- examples/MartingStrategy/martingEngine.py
+++ examples/MartingStrategy/martingEngine.py
@@ -14,7 +14,8 @@ from vnpy.trader.object import BarData
 from vnpy.trader.constant import Direction, Exchange
 
 # from martingStrategy_general import MartingPortfolio
-from martingStrategy_trending import MartingPortfolio
+from martingStrategy_trending_test import MartingPortfolio
+# from martingStrategy_trending import MartingPortfolio
 
 from vnpy.app.cta_strategy.base import DAILY_DB_NAME, MINUTE_DB_NAME, HOUR_DB_NAME, MinuteDataBaseName, HourDataBaseName
 import pandas as pd
