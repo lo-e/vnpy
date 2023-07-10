35430686d50eea9d4f149fb7c20b1a7251ba38f2
loe <lo-e@outlook.com>
2023-07-10 13:06:00 +0800

    upadte

--- examples/MartingStrategy/run.py
+++ examples/MartingStrategy/run.py
@@ -26,7 +26,7 @@ def one():
     # 回测起始日期
     engine = BacktestingEngine()
     start_dt = datetime(2022, 1, 1)
-    end_dt = datetime(2023, 7, 9)
+    end_dt = datetime(2023, 7, 10)
     engine.setPeriod(start_dt, end_dt)
     figSavedName = ""
     if figSavedName:
