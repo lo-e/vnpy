85fa6ba174a60501ad0dc82f4ff50379f6c87fbd
loe <lo-e@outlook.com>
2023-07-12 19:45:31 +0800

    update

--- examples/vn_trader/App/marting/base.py
+++ examples/vn_trader/App/marting/base.py
@@ -4,3 +4,10 @@ Defines constants and objects used in CtaStrategy App.
 
 APP_NAME = "Marting"
 EVENT_MARTING_PORTFOLIO = 'eMartingPortfolio.'    # 马丁组合状态变化事件
+
+from enum import Enum
+class StrategyExecuteMode(Enum):
+    # 策略执行模式
+    FORWARD_ONLY = "FORWARD_ONLY" # 只进行趋势追踪
+    INVERSE_ONLY = "INVERSE_ONLY" # 只进行趋势反转
+    FORWARD_INVERSE = "FORWARD_INVERSE" # 同时进行趋势追踪和趋势反转
--- examples/vn_trader/App/marting/engine.py
+++ examples/vn_trader/App/marting/engine.py
@@ -62,13 +62,7 @@ import re
 from collections import OrderedDict
 from time import sleep
 from decimal import Decimal
-from enum import Enum
-
-class StrategyExecuteMode(Enum):
-    # 策略执行模式
-    FORWARD_ONLY = "FORWARD_ONLY" # 只进行趋势追踪
-    INVERSE_ONLY = "INVERSE_ONLY" # 只进行趋势反转
-    FORWARD_INVERSE = "FORWARD_INVERSE" # 同时进行趋势追踪和趋势反转
+from .base import StrategyExecuteMode
 
 STOP_STATUS_MAP = {
     Status.SUBMITTING: StopOrderStatus.WAITING,
