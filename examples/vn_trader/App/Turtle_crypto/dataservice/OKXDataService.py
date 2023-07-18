05d84f5bc023738a206757190109ecb730bb13c6
loe <lo-e@outlook.com>
2023-07-19 02:12:18 +0800

    update

--- examples/vn_trader/App/Turtle_crypto/dataservice/OKXDataService.py
+++ examples/vn_trader/App/Turtle_crypto/dataservice/OKXDataService.py
@@ -137,32 +137,38 @@ def okx_get_symbol_list(type:OKXType=OKXType.USDT, need_data: bool = False):
     else:
         return symbol_list
 
-def okx_get_first_bar_datetime(symbol:str, interval:str, start_time:str=''):
+def okx_get_first_bar_datetime(symbol:str, interval:str, from_time:str=''):
     result = None
-    params: dict = {
-        "symbol": symbol,
-        "interval": interval,
-        "limit": 10
-    }
 
+    if not from_time:
+        from_time = "2020-01-01 00:00:00"
+    
     api = '/api/v5/market/history-candles'
-    base_url = f'{main_url}{api}'
+    base_url = f'{main_url}{api}?instId={symbol}&bar={interval}&limit=100'
 
-    url = base_url
-    if start_time:
-        timeArray = time.strptime(start_time, "%Y-%m-%d %H:%M:%S")
-        start_time = int(time.mktime(timeArray))
-        params["startTime"] = start_time * 1000
+    until = from_time
+    until_ts = ''
+    while True:
+        if interval == "1m":
+            until = (datetime.strptime(until, "%Y-%m-%d %H:%M:%S") + timedelta(minutes=100)).strftime("%Y-%m-%d %H:%M:%S")
+        
+        elif interval == "1D":
+            until = (datetime.strptime(until, "%Y-%m-%d %H:%M:%S") + timedelta(days=100)).strftime("%Y-%m-%d %H:%M:%S")
+        timeArray = time.strptime(until, "%Y-%m-%d %H:%M:%S")
+        until_ts = int(time.mktime(timeArray)) * 1000
 
-    resp = requests.get(url, headers={}, params=params)
-    bar_data_list = resp.json()
-
-    if bar_data_list:
-        # 数据整理
-        for data in bar_data_list:
-            ts = data[0]
-            result = datetime.fromtimestamp(int(ts) / 1000)
+        url = base_url + f'&after={until_ts}'
+        resp = requests.get(url, headers={}, params={})
+        data = resp.json()
+        bar_data_list = data.get('data', [])
+        if bar_data_list:
+            # 转换时间戳
+            ts = bar_data_list[0][0]
+            the_timestamp = int(ts) / 1000
+            datetime_str = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(the_timestamp))
+            result = datetime.strptime(datetime_str, "%Y-%m-%d %H:%M:%S")
             break
+
     return result
 
 def get_csv_path():
@@ -172,11 +178,19 @@ def get_csv_path():
     return csv_path
 
 if __name__ == '__main__':
-    #"""
+    """
     symbol = 'BTC-USDT-SWAP'
     interval = '1m'
     from_time = ''
     from_time = (datetime.now() - timedelta(days=1)).strftime("%Y-%m-%d %H:%M:%S")
     okx_get_bar_data(symbol=symbol, interval=interval, from_time=from_time, limit=100)
     print('completed！')
+    """
+
+    #"""
+    # 获取合约从某个时间开始最早的交易时间
+    symbol = 'BTC-USDT-SWAP'
+    interval = '1m'
+    from_time = "2019-01-01 00:00:00"
+    okx_get_first_bar_datetime(symbol=symbol, interval=interval, from_time=from_time)
     #"""
