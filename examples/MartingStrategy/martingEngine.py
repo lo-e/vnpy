9a729f73990927e0992ad778595be94c1dbae708
loe <lo-e@outlook.com>
2023-07-27 14:46:24 +0800

    update

--- examples/MartingStrategy/martingEngine.py
+++ examples/MartingStrategy/martingEngine.py
@@ -87,7 +87,6 @@ class BacktestingEngine(object):
                 self.target_symbol_list = ['ALGOUSDT.BINANCE', 'DYDXUSDT.BINANCE', 'ENSUSDT.BINANCE', 'EOSUSDT.BINANCE', 'SUSHIUSDT.BINANCE']
                 # self.target_symbol_list = ['ALGOUSDT.BINANCE', 'CHRUSDT.BINANCE', 'DYDXUSDT.BINANCE', 'ENSUSDT.BINANCE', 'EOSUSDT.BINANCE']
                 # self.target_symbol_list = ['ALGOUSDT.BINANCE', 'CHRUSDT.BINANCE', 'DYDXUSDT.BINANCE', 'EOSUSDT.BINANCE', 'SUSHIUSDT.BINANCE']
-                self.target_symbol_list = ["BCHUSDT.BINANCE", "DOGEUSDT.BINANCE", "COMPUSDT.BINANCE", "LINKUSDT.BINANCE", "XRPUSDT.BINANCE"]
                 self.target_symbol_list = []
             
             elif exchange == "OKX":
