76292004e7245ce1312694a07756dbc25e6c7673
loe <lo-e@outlook.com>
2023-08-03 17:43:23 +0800

    update

--- examples/MartingStrategy/run.py
+++ examples/MartingStrategy/run.py
@@ -495,7 +495,7 @@ def combine_backtesting():
     
     # 回测时间
     start_dt = datetime(2022, 1, 1)
-    end_dt = datetime(2023, 8, 5)
+    end_dt = datetime(2023, 8, 2)
 
     # 获取历史数据
     start_dt_str = start_dt.strftime("%Y-%m-%d")
