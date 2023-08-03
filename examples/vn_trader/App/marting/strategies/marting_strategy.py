8fe1fe9f0561556053eee69f6eb54cbec136e9f0
loe <lo-e@outlook.com>
2023-08-03 12:01:17 +0800

    update

--- examples/vn_trader/App/marting/strategies/marting_strategy.py
+++ examples/vn_trader/App/marting/strategies/marting_strategy.py
@@ -24,7 +24,7 @@ REDUCE_RATE = 0.005 # 盈利平仓比率
 CONTINUOUS_INCREASE_RATE = 0.01 # 持续加仓比率
 TRENDING_INCREASE_RATE = 0.04 # 趋势加仓比率
 TRENDING_OPEN_LOSS_RATE = 0.02 # 趋势加仓时的持仓亏损比率
-TOP_STEP = 6
+TOP_STEP = 3
 
 class MartingStrategy(CtaTemplate):
     """马丁策略"""
