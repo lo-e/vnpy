fa4f4b927c1736b629eae213522fcd1be5722438
loe <lo-e@outlook.com>
2023-08-05 17:52:26 +0800

    update

--- examples/MartingStrategy/utility.py
+++ examples/MartingStrategy/utility.py
@@ -511,7 +511,7 @@ def generate_setting(symbol_open_dict: dict, marting_type:str, exchange:str):
 
     # INVERSE目标合约
     # target_symbols = ['TRXUSDT.BINANCE', 'SUSHIUSDT.BINANCE', 'ENSUSDT.BINANCE', 'CHRUSDT.BINANCE', 'ALGOUSDT.BINANCE', 'EOSUSDT.BINANCE', 'DYDXUSDT.BINANCE', 'ATOMUSDT.BINANCE']
-    target_symbols = ['DOGEUSDT.BINANCE', 'DOTUSDT.BINANCE', 'ETCUSDT.BINANCE', 'AAVEUSDT.BINANCE', 'ADAUSDT.BINANCE', 'BANDUSDT.BINANCE']
+    target_symbols = ['DOGEUSDT.BINANCE', 'DOTUSDT.BINANCE', 'ETCUSDT.BINANCE', 'AAVEUSDT.BINANCE', 'ADAUSDT.BINANCE', 'AXSUSDT.BINANCE']
 
     # 获取合约最小交易价值
     symbol_min_value_dict = {}
