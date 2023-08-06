23e54b180e9053e65d9cc3ec605a83bcd34916b2
loe <lo-e@outlook.com>
2023-08-06 11:29:40 +0800

    update

--- examples/MartingStrategy/martingEngine.py
+++ examples/MartingStrategy/martingEngine.py
@@ -72,8 +72,7 @@ class BacktestingEngine(object):
         self.portfolio = MartingDCAPortfolio(self)
         if exchange == "BINANCE":
             self.target_symbol_list = []
-            # self.target_symbol_list = ['1000SHIBUSDT.BINANCE', 'IMXUSDT.BINANCE', 'OGNUSDT.BINANCE']
-            # self.target_symbol_list = ['BCHUSDT.BINANCE', 'EOSUSDT.BINANCE', 'DOGEUSDT.BINANCE', 'DOTUSDT.BINANCE', 'BANDUSDT.BINANCE']
+            self.target_symbol_list = ['AAVEUSDT.BINANCE', 'ADAUSDT.BINANCE', 'AXSUSDT.BINANCE']
         
         elif exchange == "OKX":
             self.target_symbol_list = ['ALGO-USDT-SWAP.OKX', 'DYDX-USDT-SWAP.OKX', 'ENS-USDT-SWAP.OKX', 'EOS-USDT-SWAP.OKX', 'SUSHI-USDT-SWAP.OKX']
--- examples/MartingStrategy/run.py
+++ examples/MartingStrategy/run.py
@@ -26,7 +26,7 @@ def backtesting():
     # 回测起始日期
     engine = BacktestingEngine()
     start_dt = datetime(2023, 1, 1)
-    end_dt = datetime(2023, 8, 5)
+    end_dt = datetime(2023, 8, 9)
     engine.setPeriod(start_dt, end_dt)
     figSavedName = ""
     if figSavedName:
@@ -717,7 +717,7 @@ def combine(l, n):
 
 if __name__ == "__main__":
     # 合约列表回测
-    # backtesting()
+    backtesting()
 
     # 随机组合合约列表回测
-    combine_backtesting()
+    # combine_backtesting()
