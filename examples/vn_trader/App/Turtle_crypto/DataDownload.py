df4cd9e2b6d92499570c2118a46676217c34162f
loe <lo-e@outlook.com>
2023-08-09 10:11:47 +0800

    update

--- examples/vn_trader/App/Turtle_crypto/DataDownload.py
+++ examples/vn_trader/App/Turtle_crypto/DataDownload.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
         mode = input("选择模式（默认1）【接口获取：1 正向：2 反向：3】")
         if mode == "2":
             contract_list = ["BTC-USDT-SWAP", "ETH-USDT-SWAP"]
-            contract_list = ['LINK-USDT-SWAP']
+            contract_list = ['AAVE-USDT-SWAP', 'ADA-USDT-SWAP', 'AXS-USDT-SWAP', 'DOGE-USDT-SWAP', 'LINK-USDT-SWAP', 'BNB-USDT-SWAP', 'UNI-USDT-SWAP']
 
         elif mode == "3":
             contract_list = ["BTC-USD-SWAP", "ETH-USD-SWAP"]
