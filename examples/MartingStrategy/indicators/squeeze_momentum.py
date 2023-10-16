adf00b7152b8e4bcb80c399920744ff8c9e695bb
loe_mipro <lo-e@outlook.com>
2023-10-16 15:36:51 +0800

    a

--- examples/MartingStrategy/indicators/squeeze_momentum.py
+++ examples/MartingStrategy/indicators/squeeze_momentum.py
@@ -1,33 +1,77 @@
-from mimetypes import inited
+from pyrsistent import v
 from vnpy.trader.object import BarData
 from vnpy.trader.utility import ArrayManager
+from vnpy.trader.constant import Direction
+import talib
 import numpy as np
+from pymongo import MongoClient
+from vnpy.app.cta_strategy.base import MinuteDataBaseName
+from datetime import datetime
 
 class SqueezeMomentum(object):
-    def __init__(self, 
-                 bb_length:int=20,
-                 bb_factor:2.0,
-                 kc_length:int=20,
-                 kc_factor:1.5) -> None:
-        self.bb_length:int = bb_length
-        self.bb_factor:int = bb_factor
+    def __init__(
+        self,
+        bb_length: int = 20,
+        bb_factor: int = 2.0,
+        kc_length: int = 20,
+        kc_factor: int = 1.5,
+    ) -> None:
+        self.bb_length: int = bb_length
+        self.bb_factor: int = bb_factor
         self.kc_length: int = kc_length
-        self.kc_factor:int = kc_factor
+        self.kc_factor: int = kc_factor
 
-        self.size = max(bb_length, kc_length) + 1
-        self.array_manager = ArrayManager(self.size)
-    
+        self.inited = False
+        self.array_manager = ArrayManager(max(bb_length, kc_length) + 1)
+
     def update_bar(self, bar: BarData) -> None:
-        if self.array_manager.inited:
+        self.array_manager.update_bar(bar)
+        self.inited = self.array_manager.inited
+
+    def generate_signal(self) -> Direction:
+        direction = Direction.NET
+        if self.inited:
             # 计算布林带通道
-            basis = np.mean(self.close_array[-self.bb_length:])
-            dev = self.bb_factor * np.std(self.close_array[-self.bb_length:])
-            upper_bb = basis + dev
-            lower_bb = basis - dev
+            upper_bb, lower_bb = self.array_manager.boll(self.bb_length, self.bb_factor)
 
             # 计算肯特纳通道
-            ma = np.mean(self.close_array[-self.kc_length:])
-            range = useTrueRange ? tr : (high - low)
-            rangema = sma(range, lengthKC)
-            upperKC = ma + rangema * multKC
-            lowerKC = ma - rangema * multKC
+            upper_kc, lower_kc = self.array_manager.keltner(
+                self.kc_length, self.kc_factor
+            )
+
+            # 挤压状态
+            sqz_on = (lower_bb > lower_kc) and (upper_bb < upper_kc)
+            sqz_off = (lower_bb < lower_kc) and (upper_bb > upper_kc)
+            no_sqz = (not sqz_on) and (not sqz_off)
+
+            # 动量指标
+            high = np.max(self.array_manager.high_array[-self.kc_length :])
+            low = np.min(self.array_manager.low_array[-self.kc_length :])
+            sma = self.array_manager.sma(self.kc_length)
+            avg = (((high + low) / 2.0) + sma) / 2.0
+            val = talib.LINEARREG(self.array_manager.close_array - avg, self.kc_length)
+
+        return direction
+
+
+if __name__ == "__main__":
+    indicator = SqueezeMomentum(bb_length=3,
+                                bb_factor=2,
+                                kc_length=3,
+                                kc_factor=1.5)
+    symbol = f"BTCUSDT.BINANCE"
+    start_dt = datetime.strptime(f"2023-10-01 00:00:00", f"%Y-%m-%d %H:%M:%S")
+    end_dt = datetime.strptime(f"2023-12-31 00:00:00", f"%Y-%m-%d %H:%M:%S")
+    
+    mc = MongoClient()
+    db = mc[MinuteDataBaseName(5)]
+    flt = {'datetime':{'$gte':start_dt,
+                       '$lte':end_dt}}
+    collection = db[symbol]
+    cursor = collection.find(flt).sort('datetime')
+    for d in cursor:
+        bar = BarData(gateway_name = "", symbol = "", exchange = None, datetime = None)
+        bar.__dict__ = d
+        indicator.update_bar(bar)
+        signal = indicator.generate_signal()
+
