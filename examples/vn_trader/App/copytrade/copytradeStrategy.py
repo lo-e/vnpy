01e49f8db52af5ea23c4a93a4a920db274f7c70e
loe <lo-e@outlook.com>
2023-11-12 23:25:47 +0800

    a

--- examples/vn_trader/App/copytrade/copytradeStrategy.py
+++ examples/vn_trader/App/copytrade/copytradeStrategy.py
@@ -10,8 +10,8 @@ from vnpy.trader.utility import ArrayManager
 from vnpy.app.cta_strategy.base import *
 from datetime import datetime, timedelta
 from vnpy.trader.constant import Interval
-from vnpy.trader.object import BarData, TickData
-from vnpy.trader.utility import round_to, floor_to, ceil_to
+from vnpy.trader.object import BarData, TickData, PositionData
+from vnpy.trader.utility import round_to, floor_to, ceil_to, load_json_path
 import numpy as np
 from threading import Thread
 from utilities.BarGenerator import BarGenerator
@@ -19,6 +19,9 @@ from App.marting.martingPortfolio import BAR_DOWNLOAD_GENERATE_COMPLETE
 from vnpy.event import Event
 from copy import copy
 from vnpy.trader.event import EVENT_MAINENGINE_POSITION_UPDATED
+import os
+from pathlib import Path
+from vnpy.trader.constant import Exchange
 
 class CopytradeStrategy(CtaTemplate):
     """ 跟单交易策略 """
@@ -44,14 +47,22 @@ class CopytradeStrategy(CtaTemplate):
         # 合约仓位字典
         self.symbol_pos_dict = {}
 
+        # 跟单设置
+        self.copy_setting = {}
+
         # 完成setting.json参数的配置
         super(CopytradeStrategy, self).__init__(
             cta_engine=ctaEngine, strategy_name="", vt_symbol="", setting=setting
         )
 
     def on_init(self):
+        # 订阅交易所仓位更新
         self.cta_engine.event_engine.register(EVENT_MAINENGINE_POSITION_UPDATED, self.on_mainengine_position_updated)
-        pass
+
+        # 导入跟单设置
+        dir_path = Path(os.path.dirname(os.path.realpath(__file__)))
+        file_path = dir_path.joinpath("setting.json")
+        self.copy_setting = load_json_path(file_path)
 
     def on_start(self):
         pass
@@ -62,8 +73,9 @@ class CopytradeStrategy(CtaTemplate):
     def on_mainengine_position_updated(self, event):
         oms_engine = self.cta_engine.main_engine.engines["oms"]
         for vt_positionid, position in oms_engine.positions.items():
-            exchange = position.exchange
-            exhcange_user = position.exchange_user
+            exchange: Exchange = position.exchange
+            exchange_user: str = position.exchange_user
+            target_setting = self.copy_setting.get(exchange.value, {}).get(exchange_user, {})
             print(f"{datetime.now()}\t{vt_positionid}\t{position.volume}\t{position.price}")
         print(f"\n")
 
