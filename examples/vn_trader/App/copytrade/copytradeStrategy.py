7526f51cca11ca3536f8bc4f25b0d6fabbc96cf8
loe <lo-e@outlook.com>
2024-01-21 11:31:06 +0800

    a

--- examples/vn_trader/App/copytrade/copytradeStrategy.py
+++ examples/vn_trader/App/copytrade/copytradeStrategy.py
@@ -350,7 +350,7 @@ class CopytradeStrategy(CtaTemplate):
                     墙头草 'D5E7A8430A35CA84'
                     ALvinnn111 '9B28742D954561AE'
                     """
-                    trader_position_data = gateway.rest_api.query_copytrade(trader="9B28742D954561AE")
+                    trader_position_data = gateway.rest_api.query_copytrade(trader)
                     if isinstance(trader_position_data, list):
                         for d in trader_position_data:
                             symbol = d["instId"]
@@ -364,7 +364,9 @@ class CopytradeStrategy(CtaTemplate):
                                 if posSide == "short":
                                     pos = pos * -1
                                 print(f"{symbol}\t{posSide}\t{pos}")
-            
+
+                        trader_name = self.trader_setting.get(trader, {}).get("trader", "")
+                        print(f"{datetime.now()}\t带单员：{trader_name}\t开单数量：{len(trader_position_data)}\n")
             except Exception as e:
                 pass
             sleep(10)
