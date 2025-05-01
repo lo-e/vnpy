2e85981253e0ae1a833d92e43a1fe5393dbb1c63
loe <lo-e@outlook.com>
2025-05-01 19:15:28 +0800

    u

--- examples/vn_trader/App_Command/trending_sniper/trendingSniperPortfolio.py
+++ examples/vn_trader/App_Command/trending_sniper/trendingSniperPortfolio.py
@@ -140,88 +140,92 @@ class TrendingSniperPortfolio(object):
         # 下载Bar数据
         print_(f"Bar数据下载中..")
         self.bar_downloading = True
-        download_success = False
-        result_bar_list = []
-        try_count = 0
-        while try_count < 5:
-            try_count += 1
-            try:
-                # 按交易所分类合约
-                contract_exchange_dict = {}
-                for symbol in self.strategy_symbols.copy():
-                    exchange = symbol.split(".")[-1]
-                    exchange_symbols = contract_exchange_dict.get(exchange, set())
-                    exchange_symbols.add(symbol.split(".")[0])
-                    contract_exchange_dict[exchange] = exchange_symbols
+        try:
+            download_success = False
+            result_bar_list = []
+            try_count = 0
+            while try_count < 5:
+                try_count += 1
+                try:
+                    # 按交易所分类合约
+                    contract_exchange_dict = {}
+                    for symbol in self.strategy_symbols.copy():
+                        exchange = symbol.split(".")[-1]
+                        exchange_symbols = contract_exchange_dict.get(exchange, set())
+                        exchange_symbols.add(symbol.split(".")[0])
+                        contract_exchange_dict[exchange] = exchange_symbols
 
-                # 先清空历史下载数据 
-                self.download_engine.delete_history_data(target_dir=self.name)
+                    # 先清空历史下载数据 
+                    self.download_engine.delete_history_data(target_dir=self.name)
 
-                # 开始下载
-                for exchange, exchange_symbols in contract_exchange_dict.items():
-                    if exchange == "BINANCE":
-                        self.download_engine.download_from_binance(
-                            contract_list=exchange_symbols, days=1, from_data_base=True, save_to=self.name, delete_history_data=False, show_progress=False
-                        )
+                    # 开始下载
+                    for exchange, exchange_symbols in contract_exchange_dict.items():
+                        if exchange == "BINANCE":
+                            self.download_engine.download_from_binance(
+                                contract_list=exchange_symbols, days=1, from_data_base=True, save_to=self.name, delete_history_data=False, show_progress=False
+                            )
 
-                    elif exchange == "OKX":
-                        self.download_engine.download_from_okx(
-                            contract_list=exchange_symbols, days=1, from_data_base=True, save_to=self.name, delete_history_data=False, show_progress=False
-                        )
-                    
-                    elif exchange == "BYBIT":
-                        self.download_engine.download_from_bybit(
-                            contract_list=exchange_symbols, days=1, from_data_base=True, save_to=self.name, delete_history_data=False, show_progress=False
-                        )
+                        elif exchange == "OKX":
+                            self.download_engine.download_from_okx(
+                                contract_list=exchange_symbols, days=1, from_data_base=True, save_to=self.name, delete_history_data=False, show_progress=False
+                            )
+                        
+                        elif exchange == "BYBIT":
+                            self.download_engine.download_from_bybit(
+                                contract_list=exchange_symbols, days=1, from_data_base=True, save_to=self.name, delete_history_data=False, show_progress=False
+                            )
 
-                # 检查下载结果
-                all_downloaded = True
-                for symbol in self.strategy_symbols.copy():
-                    client = MongoClient("localhost", 27017)
-                    db = client[MINUTE_DB_NAME]
-                    collection = db[symbol]
+                    # 检查下载结果
+                    all_downloaded = True
+                    for symbol in self.strategy_symbols.copy():
+                        client = MongoClient("localhost", 27017)
+                        db = client[MINUTE_DB_NAME]
+                        collection = db[symbol]
 
-                    now = datetime.now().replace(second=0, microsecond=0)
-                    dt_from = now - timedelta(minutes=10)
-                    dt_to = now - timedelta(minutes=5)
-                    flt = {"datetime": {"$gte": dt_from}}
-                    bar_list = list(collection.find(flt).sort("datetime", DESCENDING))
-                    if bar_list:
-                        data = bar_list[0]
-                        bar = BarData(
-                            gateway_name="",
-                            symbol="",
-                            exchange=Exchange.NONE,
-                            datetime=None,
-                            endDatetime=None)
-                        bar.__dict__ = data
-                        result_bar_list.append(copy(bar))
-                        if bar.datetime < dt_to:
-                            all_downloaded = False
-                            break
+                        now = datetime.now().replace(second=0, microsecond=0)
+                        dt_from = now - timedelta(minutes=10)
+                        dt_to = now - timedelta(minutes=5)
+                        flt = {"datetime": {"$gte": dt_from}}
+                        bar_list = list(collection.find(flt).sort("datetime", DESCENDING))
+                        if bar_list:
+                            data = bar_list[0]
+                            bar = BarData(
+                                gateway_name="",
+                                symbol="",
+                                exchange=Exchange.NONE,
+                                datetime=None,
+                                endDatetime=None)
+                            bar.__dict__ = data
+                            result_bar_list.append(copy(bar))
+                            if bar.datetime < dt_to:
+                                all_downloaded = False
+                                break
 
-                download_success = all_downloaded
-                break
+                    download_success = all_downloaded
+                    break
 
-            except Exception as e:
-                msg = f"TrendingSniperPortfolio 下载Bar数据出错\n\n{e}"
+                except Exception as e:
+                    msg = f"TrendingSniperPortfolio 下载Bar数据出错\n\n{e}"
+                    self.send_ding_talk(msg)
+
+            if download_success:
+                # 输出结果
+                # for bar in result_bar_list:
+                #     print(f"{bar.datetime}\t{bar.vt_symbol}\t{bar.open_price}\t{bar.high_price}\t{bar.low_price}\t{bar.close_price}")
+
+                msg = f"Bar数据已更新！（{len(result_bar_list)}）\n"
+                print_(msg)
+
+                # 发送事件
+                event = Event(type=EVENT_BAR_UPDATED, data="")
+                self.cta_engine.event_engine.put(event)
+
+            else:
+                msg = f"TrendingSniperPortfolio Bar数据下载缺失！"
                 self.send_ding_talk(msg)
 
-        if download_success:
-            # 输出结果
-            # for bar in result_bar_list:
-            #     print(f"{bar.datetime}\t{bar.vt_symbol}\t{bar.open_price}\t{bar.high_price}\t{bar.low_price}\t{bar.close_price}")
-
-            msg = f"Bar数据已更新！（{len(result_bar_list)}）\n"
-            print_(msg)
-
-            # 发送事件
-            event = Event(type=EVENT_BAR_UPDATED, data="")
-            self.cta_engine.event_engine.put(event)
-
-        else:
-            msg = f"TrendingSniperPortfolio Bar数据下载缺失！"
-            self.send_ding_talk(msg)
+        except Exception as e:
+            pass
 
         self.bar_downloading = False
 
@@ -233,63 +237,67 @@ class TrendingSniperPortfolio(object):
     def download_instruments_data(self):
         # 下载合约列表数据
         self.instruments_downloading = True
-        okx_instruments_data = []
-        okx_new = []
-        binance_instruments_data = []
-        binance_new = []
-        bybit_instruments_data = []
-        bybit_new = []
+        try:
+            okx_instruments_data = []
+            okx_new = []
+            binance_instruments_data = []
+            binance_new = []
+            bybit_instruments_data = []
+            bybit_new = []
 
-        download_success = False
-        try_count = 0
-        while try_count < 5:
-            try_count += 1
-            try:
-                if not okx_instruments_data:
-                    okx_history_instruments_data = self.exchange_instruments_data.get(Exchange.OKX.value, {})
-                    okx_instruments_data, okx_new = self.download_engine.download_instruments_list(Exchange.OKX, okx_history_instruments_data)
-                
-                if not binance_instruments_data:
-                    binance_history_instruments_data = self.exchange_instruments_data.get(Exchange.BINANCE.value, {})
-                    binance_instruments_data, binance_new = self.download_engine.download_instruments_list(Exchange.BINANCE, binance_history_instruments_data)
+            download_success = False
+            try_count = 0
+            while try_count < 5:
+                try_count += 1
+                try:
+                    if not okx_instruments_data:
+                        okx_history_instruments_data = self.exchange_instruments_data.get(Exchange.OKX.value, {})
+                        okx_instruments_data, okx_new = self.download_engine.download_instruments_list(Exchange.OKX, okx_history_instruments_data)
+                    
+                    if not binance_instruments_data:
+                        binance_history_instruments_data = self.exchange_instruments_data.get(Exchange.BINANCE.value, {})
+                        binance_instruments_data, binance_new = self.download_engine.download_instruments_list(Exchange.BINANCE, binance_history_instruments_data)
 
-                if not bybit_instruments_data:
-                    bybit_history_instruments_data = self.exchange_instruments_data.get(Exchange.BYBIT.value, {})
-                    bybit_instruments_data, bybit_new = self.download_engine.download_instruments_list(Exchange.BYBIT, bybit_history_instruments_data)
+                    if not bybit_instruments_data:
+                        bybit_history_instruments_data = self.exchange_instruments_data.get(Exchange.BYBIT.value, {})
+                        bybit_instruments_data, bybit_new = self.download_engine.download_instruments_list(Exchange.BYBIT, bybit_history_instruments_data)
 
-                if len(okx_instruments_data) and len(binance_instruments_data) and len(bybit_instruments_data):
-                    download_success = True
-                    break
+                    if len(okx_instruments_data) and len(binance_instruments_data) and len(bybit_instruments_data):
+                        download_success = True
+                        break
 
-            except Exception as e:
-                msg = f"HitNewPortfolio 下载合约列表数据出错\n\n{e}"
+                except Exception as e:
+                    msg = f"HitNewPortfolio 下载合约列表数据出错\n\n{e}"
+                    self.send_ding_talk(msg)
+
+            # 交易所合约上新，更新Gateway合约列表、更新订阅
+            update_contract_gateway_names = set()
+            if okx_new:
+                update_contract_gateway_names.add("OKX")
+            
+            if binance_new:
+                update_contract_gateway_names.add("BINANCE")
+
+            if bybit_new:
+                update_contract_gateway_names.add("BYBIT")
+
+            if len(update_contract_gateway_names):
+                # gateway更新合约
+                self.query_gateway_contract(list(update_contract_gateway_names))
+
+                # 组合策略更新合约
+                self.update_strategy_symbols()
+
+            if download_success:
+                msg = f"合约列表数据已更新！\n"
+                print_(msg)
+            
+            else:
+                msg = f"HitNewPortfolio 下载合约列表数据失败"
                 self.send_ding_talk(msg)
-
-        # 交易所合约上新，更新Gateway合约列表、更新订阅
-        update_contract_gateway_names = set()
-        if okx_new:
-            update_contract_gateway_names.add("OKX")
         
-        if binance_new:
-            update_contract_gateway_names.add("BINANCE")
-
-        if bybit_new:
-            update_contract_gateway_names.add("BYBIT")
-
-        if len(update_contract_gateway_names):
-            # gateway更新合约
-            self.query_gateway_contract(list(update_contract_gateway_names))
-
-            # 组合策略更新合约
-            self.update_strategy_symbols()
-
-        if download_success:
-            msg = f"合约列表数据已更新！\n"
-            print_(msg)
-        
-        else:
-            msg = f"HitNewPortfolio 下载合约列表数据失败"
-            self.send_ding_talk(msg)
+        except Exception as e:
+            pass
 
         self.instruments_downloading = False
         
