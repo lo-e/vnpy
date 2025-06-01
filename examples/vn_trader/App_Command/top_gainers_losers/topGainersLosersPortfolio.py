7e99011b7c679b74e172467c89b86403fc974189
loe <lo-e@outlook.com>
2025-06-01 09:27:42 +0800

    u

--- examples/vn_trader/App_Command/top_gainers_losers/topGainersLosersPortfolio.py
+++ examples/vn_trader/App_Command/top_gainers_losers/topGainersLosersPortfolio.py
@@ -190,7 +190,7 @@ class TopGainersLosersPortfolio(object):
         exchange = ""
         exchange_user = ""
 
-        filter_tokens = ["USDC", "USDT", "DAI"]
+        filter_tokens = ["USDC", "USDT", "USDE", "USD1", "PYUSD", "USDS", "DAI"]
         if token not in filter_tokens:
             okx_symbols = list(self.exchange_instruments_data.get("OKX", {}).keys())
             symbol = f"{token}-USDT-SWAP"
