bdc446a1da1f3c64b6fe641f772400b2e19cf233
loe <lo-e@outlook.com>
2025-06-04 10:44:43 +0800

    u

--- examples/vn_trader/App_Command/top_gainers_losers/topGainersLosersPortfolio.py
+++ examples/vn_trader/App_Command/top_gainers_losers/topGainersLosersPortfolio.py
@@ -222,15 +222,15 @@ class TopGainersLosersPortfolio(object):
             if symbol in okx_symbols:
                 vt_symbol = f"{symbol}.OKX"
                 exchange = "OKX"
-                exchange_user = "lo-e(test)"
+                exchange_user = "lo-e"
 
-            # if not vt_symbol:
-            #     bybit_symbols = list(self.exchange_instruments_data.get("BYBIT", {}).keys())
-            #     symbol = f"{token}USDT"
-            #     if symbol in bybit_symbols:
-            #         vt_symbol = f"{symbol}.BYBIT"
-            #         exchange = "BYBIT"
-            #         exchange_user = "loesuperman"
+            if not vt_symbol:
+                bybit_symbols = list(self.exchange_instruments_data.get("BYBIT", {}).keys())
+                symbol = f"{token}USDT"
+                if symbol in bybit_symbols:
+                    vt_symbol = f"{symbol}.BYBIT"
+                    exchange = "BYBIT"
+                    exchange_user = "loesuperman"
 
             # if not vt_symbol:
             #     binance_symbols = list(self.exchange_instruments_data.get("BINANCE", {}).keys())
