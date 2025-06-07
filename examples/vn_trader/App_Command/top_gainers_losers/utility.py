d302982664c267e2089e5d6960733f0687a5c50c
loe <lo-e@outlook.com>
2025-06-07 12:47:24 +0800

    u

--- examples/vn_trader/App_Command/top_gainers_losers/utility.py
+++ examples/vn_trader/App_Command/top_gainers_losers/utility.py
@@ -11,6 +11,9 @@ import os
 from vnpy.trader.utility import DIR_SYMBOL
 import pandas as pd
 from datetime import datetime
+from queue import Queue
+import socket
+from dingtalkchatbot.chatbot import DingtalkChatbot
 
 class Chrome(object):
     def __init__(self, cta_engine) -> None:
@@ -397,6 +400,14 @@ class Chrome(object):
             df = pd.DataFrame(up_down_list)
             df.to_csv(up_down_file_path, index=False)
 
+            if abs(mean_up_down_change) >= abs(mean_down_up_change) * 2:
+                msg = f"空头过热 准备做多\n\n增幅 {mean_up_down_change}\n降幅 {mean_down_up_change}"
+                dingtalk.send_ding_talk(msg)
+
+            if abs(mean_down_up_change) >= abs(mean_up_down_change) * 2:
+                msg = f"空头过热 准备做多\n\n增幅 {mean_up_down_change}\n降幅 {mean_down_up_change}"
+                dingtalk.send_ding_talk(msg)
+
     def load_driver(self):
         # 加载浏览器
         # 获取当前文件所在路径
@@ -423,7 +434,58 @@ class Chrome(object):
         except:
             pass
 
+class DingTalkEngine(object):
+    # 发送钉钉机器人消息
+    def __init__(self):
+        """"""
+        super(DingTalkEngine, self).__init__()
+
+        self.thread = Thread(target=self.run)
+        self.queue = Queue()
+        self.active = False
+
+    def send_ding_talk(self, content):
+        # 内容添加电脑名称、时间
+        client = socket.gethostname()
+        full_content = f'{content}\n\n【{client}】\n\n{datetime.now()}'
+
+        # 开启线程
+        if not self.active:
+            self.start()
+
+        # 消息推入队列，做流控处理
+        if self.queue.qsize() < 10:
+            self.queue.put(full_content)
+
+    def run(self):
+        """"""
+        while self.active:
+            try:
+                content = self.queue.get(block=True, timeout=1)
+
+                # 发送消息
+                webhook = 'https://oapi.dingtalk.com/robot/send?access_token=c7829ba703a3e0a28fb43f40a65f68313ec3ab43324e5bad30bd2bb660f791e4'
+                ding = DingtalkChatbot(webhook)
+                ding.send_text(msg=content, is_at_all=True)
+            except:
+                pass
+            time.sleep(2)
+
+    def start(self) -> None:
+        """"""
+        self.active = True
+        self.thread.start()
+
+    def close(self) -> None:
+        """"""
+        if not self.active:
+            return
+
+        self.active = False
+        self.thread.join()
+        
 if __name__ == "__main__":
     chrome = Chrome(cta_engine=None)
+    dingtalk = DingTalkEngine()
     # Thread(target=chrome.fetch_top_gainers_losers, args=(chrome.on_top_gainers_losers, 10)).start()
     Thread(target=chrome.fetch_long_vs_short, args=(chrome.on_long_vs_short, 10)).start()
