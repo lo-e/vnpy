1a6a5e290f9079664c163e6d56f655f76f289dd0
loe_mipro <lo-e@outlook.com>
2025-07-10 16:20:26 +0800

    u

--- examples/vn_trader/App_Command/top_gainers_losers/verify_data.py
+++ examples/vn_trader/App_Command/top_gainers_losers/verify_data.py
@@ -116,9 +116,9 @@ class Backtesting(object):
     def load_recent_1h_trending_data(self):
         # 1小时趋势数据
         print(f"加载1H历史趋势数据..")
-        self.trending_tokens_24h = {}
-        # hour_time = datetime.strptime(f"2025-06-29 00:00:00", f"%Y-%m-%d %H:%M:%S")
-        hour_time = datetime.now().replace(minute=0, second=0, microsecond=0) - timedelta(days=3)
+        self.trending_tokens_1h = {}
+        hour_time = datetime.strptime(f"2025-06-29 00:00:00", f"%Y-%m-%d %H:%M:%S")
+        # hour_time = datetime.now().replace(minute=0, second=0, microsecond=0) - timedelta(days=3)
         while hour_time < datetime.now():
             current_dir = os.path.dirname(os.path.abspath(__file__))
             date = hour_time.strftime(f"%Y-%m-%d")
