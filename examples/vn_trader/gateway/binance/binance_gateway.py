384aebd7d1f99bcbd13f9923f21235ea4d2544bd
loe <lo-e@outlook.com>
2025-08-23 07:33:09 +0800

    u

--- examples/vn_trader/gateway/binance/binance_gateway.py
+++ examples/vn_trader/gateway/binance/binance_gateway.py
@@ -1190,8 +1190,8 @@ class BinanceUsdtDataWebsocketApi(WebsocketClient):
 
             # 高频行情数据过滤（按时间）
             last_ts = self.tick_ts_data.get(symbol_upper, 0)
-            # current_ts = int(time.time()*1000)
-            current_ts = timestamp
+            current_ts = int(time.time()*1000)
+            # current_ts = timestamp
             if current_ts - last_ts < 100:
                 return
             self.tick_ts_data[symbol_upper] = current_ts
