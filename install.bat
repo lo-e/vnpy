a21f37a5bda86c961ce56a64ac84d48607b12a43
loe <lo-e@outlook.com>
2025-09-17 12:13:15 +0800

    u

--- install.bat
+++ install.bat
@@ -1,7 +1,8 @@
 :: -i https://pypi.tuna.tsinghua.edu.cn/simple
 
 :: Upgrade pip & setuptools
-python -m pip install --upgrade pip setuptools
+python -m pip install --upgrade pip
+python -m pip install --upgrade setuptools（可能会出现安装报错，可省略，或者更新验证版本setuptools==68.0.0）
 
 ::Install Python Modules
 python -m pip install -r requirements.txt
