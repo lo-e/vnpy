8655c2bc2f193e7b19c3c6280f6718ff01f084c6
loe <lo-e@outlook.com>
2025-09-24 15:41:14 +0800

    u

--- examples/vn_trader/App_Command/top_gainers_losers/trending4Strategy.py
+++ examples/vn_trader/App_Command/top_gainers_losers/trending4Strategy.py
@@ -24,7 +24,7 @@ class Trending4Strategy(TrendingStrategy):
     def check_indicator_inited(self):
         if not self.target_pos:
             if self.direction == Direction.LONG:
-                if self.hour_up and self.hour_down and self.minute_15_up and self.minute_15_down and self.minute_recent_up and self.minute_recent_down and self.minute_bar.datetime.timestamp() >= self.hour_up_ts + 3 * 60 * 60:
+                if self.hour_up and self.hour_down and self.minute_15_up and self.minute_15_down and self.minute_recent_up and self.minute_recent_down and self.minute_bar.datetime.timestamp() >= self.hour_up_ts + 3 * 60 * 60 and self.minute_recent_down >= self.hour_down:
                     self.indicator_inited = True
                     self.indicator_inited_dt = self.minute_bar_dt
                 
@@ -33,7 +33,7 @@ class Trending4Strategy(TrendingStrategy):
                     self.indicator_inited_dt = ""
 
             if self.direction == Direction.SHORT:
-                if self.hour_up and self.hour_down and self.minute_15_up and self.minute_15_down and self.minute_recent_up and self.minute_recent_down and self.minute_bar.datetime.timestamp() >= self.hour_down_ts + 3 * 60 * 60:
+                if self.hour_up and self.hour_down and self.minute_15_up and self.minute_15_down and self.minute_recent_up and self.minute_recent_down and self.minute_bar.datetime.timestamp() >= self.hour_down_ts + 3 * 60 * 60 and self.minute_recent_up <= self.hour_up:
                     self.indicator_inited = True
                     self.indicator_inited_dt = self.minute_bar_dt
 
