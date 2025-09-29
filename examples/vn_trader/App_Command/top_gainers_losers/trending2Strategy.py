94fd461f7722c9046c2cabec5cd6b5582cda09eb
loe <lo-e@outlook.com>
2025-09-29 14:13:26 +0800

    u

--- examples/vn_trader/App_Command/top_gainers_losers/trending2Strategy.py
+++ examples/vn_trader/App_Command/top_gainers_losers/trending2Strategy.py
@@ -108,6 +108,10 @@ class Trending2Strategy(TrendingStrategy):
                 open_allowed = True
 
             if open_allowed:
+                # 确认open_tags
+                self.check_opent_tags()
+
+                # 仓位计算
                 self.add_unit_pos(tick.last_price)
 
                 # 发送订单
