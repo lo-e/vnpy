aeb7d071f1deb3a4fa0daecfc7277fd09a713f52
loe <lo-e@outlook.com>
2025-10-13 17:18:51 +0800

    u

--- examples/vn_trader/App_Command/top_gainers_losers/trendingMultiStrategy.py
+++ examples/vn_trader/App_Command/top_gainers_losers/trendingMultiStrategy.py
@@ -491,7 +491,7 @@ class TrendingMultiStrategy(CtaTemplate):
                                 indicator_inited = True
 
                         elif signal_name == "T2":
-                            if 5 <= recent_minutes <= 60 and stop_price and stop_price >= self.hour_up - abs(self.hour_up - self.hour_down) / 2.0 and self.hour_down <= self.minute_recent_down <= self.hour_up - abs(self.hour_up - self.hour_down) * 0.5:
+                            if 5 <= recent_minutes <= 60 and stop_price and stop_price >= self.hour_up - abs(self.hour_up - self.hour_down) / 3.0 and self.hour_down <= self.minute_recent_down <= self.hour_up - abs(self.hour_up - self.hour_down) * 0.9:
                                 indicator_inited = True
                         
                         elif signal_name == "T3":
@@ -557,7 +557,7 @@ class TrendingMultiStrategy(CtaTemplate):
                                 indicator_inited = True
 
                         elif signal_name == "T2":
-                            if 5 <= recent_minutes <= 60 and stop_price and stop_price <= self.hour_down + abs(self.hour_up - self.hour_down) / 2.0 and self.hour_up >= self.minute_recent_up >= self.hour_down + abs(self.hour_up - self.hour_down) * 0.5:
+                            if 5 <= recent_minutes <= 60 and stop_price and stop_price <= self.hour_down + abs(self.hour_up - self.hour_down) / 3.0 and self.hour_up >= self.minute_recent_up >= self.hour_down + abs(self.hour_up - self.hour_down) * 0.9:
                                 indicator_inited = True
                         
                         elif signal_name == "T3":
