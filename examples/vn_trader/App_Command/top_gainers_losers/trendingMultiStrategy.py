e69bd3480c04aba80e2e5ad8a1ca43b43566aecd
loe <lo-e@outlook.com>
2025-10-14 23:43:11 +0800

    u

--- examples/vn_trader/App_Command/top_gainers_losers/trendingMultiStrategy.py
+++ examples/vn_trader/App_Command/top_gainers_losers/trendingMultiStrategy.py
@@ -1097,7 +1097,7 @@ class TrendingMultiStrategy(CtaTemplate):
 
         # T5信号判断
         if signal.name == "T5":
-            if len(signal.signal_dt_list) >= 3:
+            if len(signal.signal_dt_list) >= 5:
                 start_data = signal.signal_dt_list[-3]
                 start_hour_up = start_data[1]
                 start_hour_down = start_data[2]
