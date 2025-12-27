982d3175ab39406a8145660f3f9ae168a8de7180
loe <lo-e@outlook.com>
2025-12-28 02:24:11 +0800

    u

--- examples/vn_trader/App_Command/top_gainers_losers/engine.py
+++ examples/vn_trader/App_Command/top_gainers_losers/engine.py
@@ -189,11 +189,11 @@ class TopGainersLosersEngine(BaseEngine):
         strategy = self.orderid_strategy_map.get(trade.vt_orderid, None)
 
         # 止损单触发非本地订单，特殊处理
-        # if not strategy:
-        #     for strategy_ in self.strategies.values():
-        #         if strategy_.vt_symbol == trade.vt_symbol and strategy_.pos != 0:
-        #             strategy = strategy_
-        #             break
+        if not strategy:
+            for strategy_ in self.strategies.values():
+                if strategy_.vt_symbol == trade.vt_symbol and strategy_.pos != 0:
+                    strategy = strategy_
+                    break
 
         if not strategy:
             return
