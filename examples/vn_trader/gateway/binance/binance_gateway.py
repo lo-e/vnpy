f388cda1004add03f95508c869745a8cca95e1e0
loe <lo-e@outlook.com>
2026-04-24 17:13:50 +0800

    u

--- examples/vn_trader/gateway/binance/binance_gateway.py
+++ examples/vn_trader/gateway/binance/binance_gateway.py
@@ -62,7 +62,7 @@ CHINA_TZ: timezone = timezone("Asia/Shanghai")
 F_REST_HOST: str = "https://fapi.binance.com"
 
 # 实盘正向合约Websocket API地址
-F_WEBSOCKET_TRADE_HOST: str = "wss://fstream.binance.com/market/ws/"
+F_WEBSOCKET_TRADE_HOST: str = "wss://fstream.binance.com/private/ws/"
 F_WEBSOCKET_DATA_HOST: str = "wss://fstream.binance.com/market/stream"
 
 # 模拟盘正向合约REST API地址
